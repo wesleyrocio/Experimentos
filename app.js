25ee587d1fc3179f20cb8439365708f9c3338c65
wesleyrocio <wesleyrocio@gmail.com>
2022-12-17 20:39:03 -0300

    t

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 console.log("rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectando ao banco de dados");
+=======
+console.log("Rodando o sistema de gerenciamento de jogos");
+>>>>>>> parent of a1db645 (atualizando o init do projeto)
